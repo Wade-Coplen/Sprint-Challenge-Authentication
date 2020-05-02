b7b9db35e74c729b830d4cc25dd298eeb25da8e9
Wade-Coplen <wadecoplen@gmail.com>
2020-05-02 09:26:04 -0700

    2. started register in auth routher.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
 
+//import bcryptjs
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const secrets = require('../config/secrets')
+
+
+//this allows us to look up joke data and them the database
+//const Users = require('../some path')
+
+//extract the the user object from the 'req.body'
+//hash the password with bcrypt and store in the user object
+//hash format = [vers][cost][salt][hash]
+
 router.post('/register', (req, res) => {
   // implement registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
 });
 
 router.post('/login', (req, res) => {
