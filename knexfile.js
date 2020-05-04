2e8bf80cd44eabfee6715325d7391554995fed86
Wade-Coplen <wadecoplen@gmail.com>
2020-05-04 16:27:32 -0700

    10. knexfile set up for testing

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,29 @@
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    connection: { 
+      filename: './database/auth.db3' 
+    },
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations',
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { 
+      directory: './database/seeds' 
+    },
   },
-};
-
-  pool: {
-    afterCreate: (conn, done) => {
-      conn.run('PRAGMA forgeign_keys = ON', done)
-    }
-  }
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/auth.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+}
